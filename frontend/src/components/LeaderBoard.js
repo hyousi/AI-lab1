02dd6f099bf31bb3e48a41b98c43b39cd87a70f7
Ze Dang <4649tz@gmail.com>
2019-04-17 08:55:02 +0800

    docs: implement LeaderBoard

--- frontend/src/components/LeaderBoard.js
+++ frontend/src/components/LeaderBoard.js
@@ -1,10 +1,38 @@
 import React from 'react';
 
-// TODO: use fetch to show leaderboard
-function LeaderBoard() {
-  return (
-      <h1>LeaderBoard</h1>
-  );
+// TODO: style leaderboard
+class LeaderBoard extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8000/api/players")
+        .then(response => {return response.json()})
+        .then(jsonData => {
+          this.setState({data: jsonData})
+        })
+  }
+
+  render() {
+    const listItems = this.state.data.map((item) =>
+      <li>{Object.values(item).join(" ")}</li>
+    );
+
+    return (
+        <div>
+          LeaderBoard
+          <ul>
+            {listItems}
+          </ul>
+        </div>
+    );
+  }
 }
 
 export default LeaderBoard;
